421faddad7d1d3cbff364336fac29d6e3327aee1
Ratneshwar <ratneshwar.jha@hcl.com>
2022-04-26 15:16:34 +0530

    first commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+           
+            Assert.AreEqual(3, 3);
+        
+        }
+    }
+}
